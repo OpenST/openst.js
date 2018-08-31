41904388c05f7dc54b4a5345238e5c0fd5a947f0
Kedar Chandrayan <kedarchandrayan@gmail.com>
2018-08-31 16:09:49 +0530

    dev env init.

--- tools/initDevEnv.js
+++ tools/initDevEnv.js
@@ -8,13 +8,10 @@ const shell = require('shelljs'),
 
 const setUpConfig = require('./config.js');
 
-const originGethFolder = setUpConfig.origin.gethFolder,
-  auxiliaryGethFolder = setUpConfig.auxiliary.gethFolder,
-  originPassphrase = 'testtest',
-  auxiliaryPassphrase = 'testtest',
+const gethFolder = setUpConfig.chain.gethFolder,
+  passphrase = 'testtest',
   hexStartsWith = '0x',
-  originPasswordFilePath = originGethFolder + '/pwd',
-  auxiliaryPasswordFilePath = auxiliaryGethFolder + '/pwd',
+  passwordFilePath = gethFolder + '/pwd',
   etherToWeiCinversion = new BigNumber(1000000000000000000);
 
 const InitDevEnv = function(params) {
@@ -22,9 +19,7 @@ const InitDevEnv = function(params) {
 
   oThis.setupRoot = params.setupRoot;
   oThis.configJsonFilePath = oThis.setupRoot + '/' + 'config.json';
-  oThis.originGethShellPath = null;
-  oThis.auxiliaryGethShellPath = null;
-  oThis.auxiliaryGethShellPathWithZeroGas = null;
+  oThis.gethShellPath = null;
 };
 
 InitDevEnv.prototype = {
@@ -43,17 +38,11 @@ InitDevEnv.prototype = {
     // create logs folder
     oThis._executeInShell('mkdir -p ' + oThis.setupRoot + '/logs');
 
-    // create config file with default content
-    oThis._executeInShell('echo {} > ' + oThis.configJsonFilePath);
-
     // init config json file
     oThis._initConfigFile();
 
-    // init value GETH
-    oThis._initOriginGeth();
-
-    // init auxiliary GETH
-    oThis._initAuxiliaryGeth();
+    // init GETH
+    oThis._initGeth();
 
     // start services
     let startWithZeroGas = true;
@@ -65,19 +54,6 @@ InitDevEnv.prototype = {
     // fund ETH
     await oThis._fundEth();
 
-    // Deploy ERC20 Token contract
-    let contractDeploymentResponse = await oThis._deployERC20Token();
-
-    let erc20TokenContractAddress = contractDeploymentResponse.receipt.contractAddress;
-    oThis._addConfig({
-      erc20TokenContractAddress: erc20TokenContractAddress
-    });
-
-    await oThis._fundERC20Token(
-      contractDeploymentResponse,
-      new BigNumber(1000000).mul(etherToWeiCinversion).toString(10)
-    );
-
     console.log('Dev env init DONE!');
   },
 
@@ -88,171 +64,91 @@ InitDevEnv.prototype = {
     oThis._executeInShell('echo {} > ' + oThis.configJsonFilePath);
 
     oThis._addConfig({
-      originChainId: setUpConfig.origin.chainId,
-      auxiliaryChainId: setUpConfig.auxiliary.chainId,
-      originNetworkId: setUpConfig.origin.networkId,
-      auxiliaryNetworkId: setUpConfig.auxiliary.networkId,
-      originGasLimit: setUpConfig.origin.gasLimit,
-      auxiliaryGasLimit: setUpConfig.auxiliary.gasLimit,
-      originGasPrice: setUpConfig.origin.gasprice,
-      auxiliaryGasPrice: setUpConfig.auxiliary.gasprice,
-      originGethRpcEndPoint: oThis._originRpc(),
-      auxiliaryGethRpcEndPoint: oThis._auxiliaryRpc(),
-      originGethWsEndPoint: oThis._originWs(),
-      auxiliaryGethWsEndPoint: oThis._auxiliaryWs()
+      chainId: setUpConfig.chain.chainId,
+      networkId: setUpConfig.chain.networkId,
+      gasLimit: setUpConfig.chain.gasLimit,
+      gasPrice: setUpConfig.chain.gasprice,
+      gethRpcEndPoint: oThis._rpcEndpoint(),
+      gethWsEndPoint: oThis._wsEndpoint()
     });
   },
 
-  _initOriginGeth: function() {
+  _initGeth: function() {
     const oThis = this;
 
-    oThis._executeInShell('mkdir -p ' + originGethFolder);
+    oThis._executeInShell('mkdir -p ' + gethFolder);
 
-    oThis._executeInShell('echo "' + originPassphrase + '" > ' + originPasswordFilePath);
+    oThis._executeInShell('echo "' + passphrase + '" > ' + passwordFilePath);
 
-    let chainOwnerOriginAddress = oThis._generateAddress(originGethFolder);
-    let originWorkerAddress = oThis._generateAddress(originGethFolder);
-    let originDeployerAddress = oThis._generateAddress(originGethFolder);
-    let ostPrimeStakerAddress = oThis._generateAddress(originGethFolder);
-    let originFacilitator = oThis._generateAddress(originGethFolder);
-    let originMiner = oThis._generateAddress(originGethFolder);
-    let originOpsAddress = oThis._generateAddress(originGethFolder);
-    let originOrganizationAddress = oThis._generateAddress(originGethFolder);
+    let chainOwnerAddress = oThis._generateAddress(gethFolder);
+    let workerAddress = oThis._generateAddress(gethFolder);
+    let deployerAddress = oThis._generateAddress(gethFolder);
+    let facilitator = oThis._generateAddress(gethFolder);
+    let miner = oThis._generateAddress(gethFolder);
+    let opsAddress = oThis._generateAddress(gethFolder);
+    let organizationAddress = oThis._generateAddress(gethFolder);
+    let wallet1 = oThis._generateAddress(gethFolder);
+    let wallet2 = oThis._generateAddress(gethFolder);
+    let ephemeralKey1 = oThis._generateAddress(gethFolder);
+    let ephemeralKey2 = oThis._generateAddress(gethFolder);
 
     oThis._modifyGenesisFile(
-      setUpConfig.origin.chainId,
-      chainOwnerOriginAddress,
-      setUpConfig.origin.allocAmount,
-      setUpConfig.origin.gasLimit,
-      setUpConfig.origin.genesisFileTemplatePath,
-      setUpConfig.origin.genesisFilePath
+      setUpConfig.chain.chainId,
+      chainOwnerAddress,
+      setUpConfig.chain.allocAmount,
+      setUpConfig.chain.gasLimit,
+      setUpConfig.chain.genesisFileTemplatePath,
+      setUpConfig.chain.genesisFilePath
     );
 
-    let initCmd = 'geth --datadir "' + originGethFolder + '" init ' + setUpConfig.origin.genesisFilePath;
-    console.log('_initOriginGeth :: Geth Init. Command:\n', initCmd);
+    let initCmd = 'geth --datadir "' + gethFolder + '" init ' + setUpConfig.chain.genesisFilePath;
+    console.log('_initGeth :: Geth Init. Command:\n', initCmd);
     oThis._executeInShell(initCmd);
 
     oThis._addConfig({
-      chainOwnerOriginAddress: chainOwnerOriginAddress,
-      originWorkerAddress: originWorkerAddress,
-      originDeployerAddress: originDeployerAddress,
-      ostPrimeStakerAddress: ostPrimeStakerAddress,
-      originFacilitator: originFacilitator,
-      originMiner: originMiner,
-      originOpsAddress: originOpsAddress,
-      originOrganizationAddress: originOrganizationAddress
+      chainOwnerAddress: chainOwnerAddress,
+      workerAddress: workerAddress,
+      deployerAddress: deployerAddress,
+      facilitator: facilitator,
+      miner: miner,
+      opsAddress: opsAddress,
+      organizationAddress: organizationAddress,
+      wallet1: wallet1,
+      wallet2: wallet2,
+      ephemeralKey1: ephemeralKey1,
+      ephemeralKey2: ephemeralKey2
     });
 
-    let logFilePath = oThis.setupRoot + '/logs/origin-geth.log';
+    let logFilePath = oThis.setupRoot + '/logs/geth.log';
     let startCmd =
-      `geth --datadir '${originGethFolder}'` +
-      ` --networkid ${setUpConfig.origin.networkId}` +
-      ` --port ${setUpConfig.origin.geth.port}` +
-      ` --mine --minerthreads 1 --targetgaslimit ${setUpConfig.origin.gasLimit} --gasprice 0x3B9ACA00` +
-      ` --rpc --rpcapi eth,net,web3,personal,txpool --rpcaddr ${setUpConfig.origin.geth.host} --rpcport ${
-        setUpConfig.origin.geth.rpcport
+      `geth --datadir '${gethFolder}'` +
+      ` --networkid ${setUpConfig.chain.networkId}` +
+      ` --port ${setUpConfig.chain.geth.port}` +
+      ` --mine --minerthreads 1 --targetgaslimit ${setUpConfig.chain.gasLimit} --gasprice 0x3B9ACA00` +
+      ` --rpc --rpcapi eth,net,web3,personal,txpool --rpcaddr ${setUpConfig.chain.geth.host} --rpcport ${
+        setUpConfig.chain.geth.rpcport
       }` +
-      ` --ws --wsapi eth,net,web3,personal,txpool --wsaddr ${setUpConfig.origin.geth.host} --wsport ${
-        setUpConfig.origin.geth.wsport
+      ` --ws --wsapi eth,net,web3,personal,txpool --wsaddr ${setUpConfig.chain.geth.host} --wsport ${
+        setUpConfig.chain.geth.wsport
       } --wsorigins '*'` +
-      ` --etherbase ${originMiner} --unlock ${originMiner} --password ${originPasswordFilePath}` +
+      ` --etherbase ${miner} --unlock ${miner} --password ${passwordFilePath}` +
       ` 2> ${logFilePath}`;
 
-    console.log('origin start command:\n', startCmd);
+    console.log('chain start command:\n', startCmd);
 
     //Create shell script
-    let originShellScriptPath = oThis.setupRoot + '/bin/run-origin.sh';
-    oThis._executeInShell('echo #!/bin/sh > ' + originShellScriptPath);
-    oThis._executeInShell(`echo "${startCmd}" >> ${originShellScriptPath}`);
+    let shellScriptPath = oThis.setupRoot + '/bin/run-chain.sh';
+    oThis._executeInShell('echo #!/bin/sh > ' + shellScriptPath);
+    oThis._executeInShell(`echo "${startCmd}" >> ${shellScriptPath}`);
 
-    oThis.originGethShellPath = originShellScriptPath;
+    oThis.gethShellPath = shellScriptPath;
   },
 
-  _initAuxiliaryGeth: function() {
+  _startServices: function() {
     const oThis = this;
 
-    oThis._executeInShell('mkdir -p ' + auxiliaryGethFolder);
-
-    oThis._executeInShell('echo "' + auxiliaryPassphrase + '" > ' + auxiliaryPasswordFilePath);
-
-    let chainOwnerAuxiliaryAddress = oThis._generateAddress(auxiliaryGethFolder);
-    let auxiliaryWorkerAddress = oThis._generateAddress(auxiliaryGethFolder);
-    let auxiliaryDeployerAddress = oThis._generateAddress(auxiliaryGethFolder);
-    let auxiliaryFacilitator = oThis._generateAddress(auxiliaryGethFolder);
-    let auxiliarySealer = oThis._generateAddress(auxiliaryGethFolder);
-    let auxiliaryOpsAddress = oThis._generateAddress(auxiliaryGethFolder);
-    let auxiliaryOrganizationAddress = oThis._generateAddress(auxiliaryGethFolder);
-
-    oThis._modifyGenesisFile(
-      setUpConfig.auxiliary.chainId,
-      chainOwnerAuxiliaryAddress,
-      setUpConfig.auxiliary.allocAmount,
-      setUpConfig.auxiliary.gasLimit,
-      setUpConfig.auxiliary.genesisFileTemplatePath,
-      setUpConfig.auxiliary.genesisFilePath,
-      auxiliarySealer
-    );
-
-    let initCmd = 'geth --datadir "' + auxiliaryGethFolder + '" init ' + setUpConfig.auxiliary.genesisFilePath;
-    console.log('_initOriginGeth :: Geth Init. Command:\n', initCmd);
-    oThis._executeInShell(initCmd);
-
-    oThis._addConfig({
-      chainOwnerAuxiliaryAddress: chainOwnerAuxiliaryAddress,
-      auxiliaryWorkerAddress: auxiliaryWorkerAddress,
-      auxiliaryDeployerAddress: auxiliaryDeployerAddress,
-      auxiliaryFacilitator: auxiliaryFacilitator,
-      auxiliarySealer: auxiliarySealer,
-      auxiliaryOpsAddress: auxiliaryOpsAddress,
-      auxiliaryOrganizationAddress: auxiliaryOrganizationAddress
-    });
-
-    let logFilePath = oThis.setupRoot + '/logs/auxiliary-geth.log';
-    let startCmd =
-      `geth --datadir '${auxiliaryGethFolder}'` +
-      ` --networkid ${setUpConfig.auxiliary.networkId}` +
-      ` --port ${setUpConfig.auxiliary.geth.port}` +
-      ` --mine --minerthreads 1 --targetgaslimit ${setUpConfig.auxiliary.gasLimit} --gasprice 0x3B9ACA00` +
-      ` --rpc --rpcapi eth,net,web3,personal,txpool --rpcaddr ${setUpConfig.auxiliary.geth.host} --rpcport ${
-        setUpConfig.auxiliary.geth.rpcport
-      }` +
-      ` --ws --wsapi eth,net,web3,personal,txpool --wsaddr ${setUpConfig.auxiliary.geth.host} --wsport ${
-        setUpConfig.auxiliary.geth.wsport
-      } --wsorigins '*'` +
-      ` --etherbase ${auxiliarySealer} --unlock ${auxiliarySealer} --password ${auxiliaryPasswordFilePath}` +
-      ` 2> ${logFilePath}`;
-
-    console.log('auxiliary start command:\n', startCmd);
-
-    //Create shell script
-    let auxiliaryShellScriptPath = oThis.setupRoot + '/bin/run-auxiliary.sh';
-    oThis._executeInShell('echo #!/bin/sh > ' + auxiliaryShellScriptPath);
-    oThis._executeInShell(`echo "${startCmd}" >> ${auxiliaryShellScriptPath}`);
-
-    //Modify start cmd to start with zero gas.
-    startCmd = startCmd.replace('--gasprice 0x3B9ACA00', '--gasprice 0x0');
-
-    //Create shell script for zero gas price.
-    let zeroGasAuxiliaryShellScriptPath = oThis.setupRoot + '/bin/run-auxiliary-with-zero-gas.sh';
-    oThis._executeInShell('echo #!/bin/sh > ' + zeroGasAuxiliaryShellScriptPath);
-    oThis._executeInShell(`echo "${startCmd}" >> ${zeroGasAuxiliaryShellScriptPath}`);
-
-    oThis.auxiliaryGethShellPath = auxiliaryShellScriptPath;
-    oThis.auxiliaryGethShellPathWithZeroGas = zeroGasAuxiliaryShellScriptPath;
-  },
-
-  _startServices: function(startWithZeroGas) {
-    const oThis = this;
-
-    //Start Auxiliary Geth
-    let auxiliaryGethShellPath = oThis.auxiliaryGethShellPath;
-    if (startWithZeroGas) {
-      auxiliaryGethShellPath = oThis.auxiliaryGethShellPathWithZeroGas;
-    }
-    shellAsyncCmd.run(`sh ${auxiliaryGethShellPath}`);
-
-    //Start Origin Geth
-    shellAsyncCmd.run(`sh ${oThis.originGethShellPath}`);
+    //Start Geth
+    shellAsyncCmd.run(`sh ${oThis.gethShellPath}`);
 
     console.log('* Sleeping for 5 seconds. Lets wait for geth to come up.');
     return new Promise(function(resolve, reject) {
@@ -268,7 +164,7 @@ InitDevEnv.prototype = {
 
     let GethChecker = require('./GethChecker');
 
-    return new GethChecker([oThis._originRpc(), oThis._auxiliaryRpc()]).perform();
+    return new GethChecker([oThis._rpcEndpoint()]).perform();
   },
 
   _fundEth: async function() {
@@ -276,18 +172,19 @@ InitDevEnv.prototype = {
 
     let configFileContent = JSON.parse(fs.readFileSync(oThis.configJsonFilePath, 'utf8'));
 
-    let senderAddr = configFileContent.chainOwnerOriginAddress,
-      web3Provider = new Web3(oThis._originRpc()),
+    let senderAddr = configFileContent.chainOwnerAddress,
+      web3Provider = new Web3(oThis._rpcEndpoint()),
       amount = '100000000000000000000';
 
     let ethRecipients = [
-      'originWorkerAddress',
-      'originDeployerAddress',
-      'ostPrimeStakerAddress',
-      'originFacilitator',
-      'originMiner',
-      'originOpsAddress',
-      'originOrganizationAddress'
+      'workerAddress',
+      'deployerAddress',
+      'facilitator',
+      'miner',
+      'opsAddress',
+      'organizationAddress',
+      'wallet1',
+      'wallet2'
     ];
 
     console.log('senderAddr', senderAddr);
@@ -302,62 +199,23 @@ InitDevEnv.prototype = {
     }
   },
 
-  _deployERC20Token: function() {
-    const oThis = this;
-
-    let configFileContent = JSON.parse(fs.readFileSync(oThis.configJsonFilePath, 'utf8'));
-
-    let deployerAddress = configFileContent.originDeployerAddress,
-      web3Provider = new Web3(oThis._originRpc());
-
-    let ERC20TokenDeployer = require('./ERC20TokenDeployer');
-
-    console.log('* Deploying ERC20 Token');
-    return new ERC20TokenDeployer({
-      web3Provider: web3Provider,
-      deployerAddress: deployerAddress,
-      deployerPassphrase: originPassphrase,
-      gasPrice: setUpConfig.origin.gasprice,
-      gasLimit: setUpConfig.origin.gasLimit
-    })
-      .perform()
-      .then(function(response) {
-        console.log('----- ERC20 Token has been deployed');
-        return response;
-      });
-  },
-
-  _fundERC20Token: function(contractDeploymentResponse, amount) {
-    const oThis = this;
-
-    let configFileContent = JSON.parse(fs.readFileSync(oThis.configJsonFilePath, 'utf8'));
-
-    let erc20TokenContract = contractDeploymentResponse.instance;
-
-    return erc20TokenContract.methods.transfer(configFileContent.ostPrimeStakerAddress, amount).send({
-      from: configFileContent.originDeployerAddress,
-      gasPrice: setUpConfig.origin.gasprice,
-      gas: setUpConfig.origin.gasLimit
-    });
-  },
-
   _fundEthFor: function(web3Provider, senderAddr, recipient, amount) {
-    return web3Provider.eth.personal.unlockAccount(senderAddr, originPassphrase).then(function() {
+    return web3Provider.eth.personal.unlockAccount(senderAddr, passphrase).then(function() {
       return web3Provider.eth.sendTransaction({
         from: senderAddr,
         to: recipient,
         value: amount,
-        gasPrice: setUpConfig.origin.gasprice,
-        gas: setUpConfig.origin.gasLimit
+        gasPrice: setUpConfig.chain.gasprice,
+        gas: setUpConfig.chain.gasLimit
       });
     });
   },
 
-  _generateAddress: function(originGethPath) {
+  _generateAddress: function(gethPath) {
     const oThis = this;
 
     let addressGerationResponse = oThis._executeInShell(
-      'geth --datadir ' + originGethPath + ' account new --password ' + originPasswordFilePath
+      'geth --datadir ' + gethPath + ' account new --password ' + passwordFilePath
     );
     return addressGerationResponse.stdout
       .replace('Address: {', hexStartsWith)
@@ -391,15 +249,6 @@ InitDevEnv.prototype = {
     let bnGasLimit = new BigNumber(gasLimit);
     fileContent.gasLimit = hexStartsWith + bnGasLimit.toString(16);
 
-    // add extra data
-    if (sealerAddress) {
-      const extraData =
-        '0x0000000000000000000000000000000000000000000000000000000000000000' +
-        sealerAddress.replace(hexStartsWith, '') +
-        '0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000';
-      fileContent.extraData = extraData;
-    }
-
     console.log(JSON.stringify(fileContent));
 
     oThis._executeInShell("echo '" + JSON.stringify(fileContent) + "' > " + chainGenesisLocation);
@@ -429,25 +278,17 @@ InitDevEnv.prototype = {
     return res;
   },
 
-  _originRpc: function() {
-    return 'http://' + setUpConfig.origin.geth.host + ':' + setUpConfig.origin.geth.rpcport;
+  _rpcEndpoint: function() {
+    return 'http://' + setUpConfig.chain.geth.host + ':' + setUpConfig.chain.geth.rpcport;
   },
 
-  _auxiliaryRpc: function() {
-    return 'http://' + setUpConfig.auxiliary.geth.host + ':' + setUpConfig.auxiliary.geth.rpcport;
-  },
-
-  _originWs: function() {
-    return 'ws://' + setUpConfig.origin.geth.host + ':' + setUpConfig.origin.geth.wsport;
-  },
-
-  _auxiliaryWs: function() {
-    return 'ws://' + setUpConfig.auxiliary.geth.host + ':' + setUpConfig.auxiliary.geth.wsport;
+  _wsEndpoint: function() {
+    return 'ws://' + setUpConfig.chain.geth.host + ':' + setUpConfig.chain.geth.wsport;
   }
 };
 
 // commander
 const os = require('os');
 new InitDevEnv({
-  setupRoot: os.homedir() + '/mosaic-setup' // later to come as argument for this script
+  setupRoot: os.homedir() + '/openst-setup' // later to come as argument for this script
 }).perform();
