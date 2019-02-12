4d50f0dd7b3c70ea3f5ce0008c14811b7344cc37
abhayks1 <abhay@ost.com>
2019-02-12 19:59:45 +0530

    Removed logging from MockContractsDeployer

--- test/utils/MockContractsDeployer.js
+++ test/utils/MockContractsDeployer.js
@@ -107,10 +107,10 @@ class MockContractsDeployer {
     return tx
       .send(txOptions)
       .on('transactionHash', function(transactionHash) {
-        console.log('\t - transaction hash:', transactionHash);
+        // console.log('\t - transaction hash:', transactionHash);
       })
       .on('error', function(error) {
-        console.log('\t !! Error !!', error, '\n\t !! ERROR !!\n');
+        // console.log('\t !! Error !!', error, '\n\t !! ERROR !!\n');
         return Promise.reject(error);
       })
       .on('receipt', function(receipt) {
@@ -118,7 +118,7 @@ class MockContractsDeployer {
       })
       .then(function(instance) {
         oThis.addresses[contractName] = instance.options.address;
-        console.log(`\t - ${contractName} Contract Address:`, instance.options.address);
+        // console.log(`\t - ${contractName} Contract Address:`, instance.options.address);
         return txReceipt;
       });
   }
