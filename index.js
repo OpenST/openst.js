b3d899482e7b9cee79979c63a37cd747f1e105ef
abhayks1 <abhay@ost.com>
2019-03-06 00:40:40 +0530

    Setup in progress

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const TokenRules = require('./lib/setup/TokenRules');
 const User = require('./lib/setup/User');
 const UserHelper = require('./lib/helper/User');
 const TokenRulesHelper = require('./lib/helper/TokenRules');
-const TokenHolder = require('./lib/helper/TokenHolder');
+const TokenHolderHelper = require('./lib/helper/TokenHolder');
 const Rules = require('./lib/setup/Rules');
 const PricerRule = require('./lib/helper/rules/PricerRule');
-const GnosisSafe = require('./lib/helper/GnosisSafe');
+const GnosisSafeHelper = require('./lib/helper/GnosisSafe');
+
+// Require Contract Interacts
+const TokenHolder = require('./lib/ContractInteract/TokenHolder');
+const GnosisSafe = require('./lib/ContractInteract/GnosisSafe');
 const Recovery = require('./lib/ContractInteract/Recovery');
+const UserWalletFactory = require('./lib/ContractInteract/UserWalletFactory');
+const ProxyFactory = require('./lib/ContractInteract/ProxyFactory');
+const CreateAndAddModules = require('./lib/ContractInteract/CreateAndAddModules');
+
+// OpenST Setup
+const OpenST = require('./lib/setup/Openst');
 
 const SignEIP1077Extension = require('./utils/SignEIP1077Extension');
 new SignEIP1077Extension();
@@ -21,16 +31,25 @@ module.exports = {
   Setup: {
     TokenRules: TokenRules,
     User: User,
-    Rules: Rules
+    Rules: Rules,
+    OpenST: OpenST
   },
   Helpers: {
     User: UserHelper,
     TokenRules: TokenRulesHelper,
-    TokenHolder: TokenHolder,
-    GnosisSafe: GnosisSafe,
+    TokenHolder: TokenHolderHelper,
+    GnosisSafe: GnosisSafeHelper,
     Recovery: Recovery,
     Rules: {
       PricerRule: PricerRule
     }
+  },
+  ContractInteract: {
+    TokenHolder: TokenHolder,
+    GnosisSafe: GnosisSafe,
+    Recovery: Recovery,
+    UserWalletFactory: UserWalletFactory,
+    ProxyFactory: ProxyFactory,
+    CreateAndAddModules: CreateAndAddModules
   }
 };
