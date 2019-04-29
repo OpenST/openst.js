00ee65df0074880a94fb0462d29c5df79ab97208
Martin Schenck <martinschenck@fastmail.com>
2019-04-29 16:12:11 +0200

    Add test for getCoGateway...Data

---
+++ test/unit/TokenHolder/getCoGatewayRedeemExecutableData.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Web3 = require('web3');
+const { assert } = require('chai');
+
+const TokenHolder = require('../../../lib/ContractInteract/TokenHolder');
+
+describe('TokenHolder.getCoGatewayRedeemExecutableData()', () => {
+  let tokenHolder;
+
+  beforeEach(() => {
+    const web3 = new Web3();
+    const address = '0x0000000000000000000000000000000000000002';
+    tokenHolder = new TokenHolder(web3, address);
+  });
+
+  it('should construct with correct parameters', async () => {
+    const amount = '100';
+    const beneficiary = '0x0000000000000000000000000000000000000005';
+    const gasPrice = '300';
+    const gasLimit = '10000000';
+    const nonce = '4';
+    const hashLock = '0x0000000000000000000000000000000000000000000000000000000000000002';
+
+    const encodedAbi = tokenHolder.getCoGatewayRedeemExecutableData(
+      amount,
+      beneficiary,
+      gasPrice,
+      gasLimit,
+      nonce,
+      hashLock
+    );
+
+    const expectedEncodedAbi =
+      '0xbf41b9d900000000000000000000000000000000000000000000000000000000000000640000000000000000000000000000000000000000000000000000000000000005000000000000000000000000000000000000000000000000000000000000012c000000000000000000000000000000000000000000000000000000000098968000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000002';
+    assert.strictEqual(
+      encodedAbi,
+      expectedEncodedAbi,
+      'The encoded ABI from TokenHolders does not matched the expected one.'
+    );
+  });
+});
